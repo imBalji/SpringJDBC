4446f5fea81d717d74bc3a8f7ed2beecef988038
Balaji <balajiaddepalli@gmail.com>
2022-02-11 23:36:53 +0530

    module-1 after fork

---
+++ src/main/java/com/pluralsight/service/RideService.java
@@ -0,0 +1,13 @@
+package com.pluralsight.service;
+
+import java.util.List;
+
+import com.pluralsight.model.Ride;
+
+public interface RideService {
+
+	List<Ride> getRides();
+
+	Ride createRide(Ride ride);
+
+}
